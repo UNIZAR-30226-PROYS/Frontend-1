90df87f1f7d476a9f0371eae3968599c9b8403fe
Gonzalo <718556@unizar.es>
2018-04-20 01:45:53 +0200

    Trabajando en el perfil del usuario y mejorados los scripts para la navegación.

---
+++ Scripts/navegacion_usuario.js
@@ -0,0 +1,32 @@
+/* Navegacion interna en el perfil de un usuario */
+$(document).ready(function(){
+    $('#profileNav').on('click','a',function(e){
+        e.preventDefault();
+
+        try{
+            var destino = $(this).attr('href');
+
+            switch(destino){
+                case 'actividad':
+                    $("#contenido-perfil").load('includes/usuario_actividad.html');
+                    break;
+                case 'listas':
+                    $("#contenido-perfil").load('includes/usuario_listas.html');
+                    break;
+                case 'canciones':
+                    $("#contenido-perfil").load('includes/usuario_canciones.html');
+                    break;
+                case 'siguiendo':
+                    $("#contenido-perfil").load('includes/usuario_siguiendo.html');
+                    break;
+                case 'seguidores':
+                    $("#contenido-perfil").load('includes/usuario_seguidores.html');
+                    break;
+            }
+        }catch($ex){
+            window.console && console.log($ex);
+        }finally{
+            return false;
+        }
+    })
+});
